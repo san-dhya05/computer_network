ffe9f9f5a86c1bdcb73334c8751527cd96483716
san-dhya05 <sandhyagharti1120@gmail.com>
2025-05-03 20:59:52 +0545

    file commit

---
+++ example.py
@@ -0,0 +1,100 @@
+
+"""name="sandhya"
+age=21
+print("my name is: ", name)
+print("my age is:",age)
+print(type(age))"""
+
+##datatypes
+"""age1=12
+age2=10.0
+a=None
+b= False
+c="string"
+print(type(age1))
+print(type(age2))
+print(type(a))
+print(type(b))"""
+
+
+##sum
+"""q=1
+w=2
+sum=q+w
+print(sum)"""
+
+
+#arithmartic
+"""a=12
+b=5
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b)
+print(a**b)"""
+
+#relational
+"""a=50
+b=30
+print(a==b)
+print(a<=b)
+print(a>=b)
+print(a!=b)"""
+
+#assignment
+"""a=20
+a+=90
+print(a)
+
+a**=5
+print(a)"""
+
+#logical
+"""print(not True)
+print(not False)
+print(True and False)
+print(True or False)"""
+
+#Type CONVERSION
+"""a=2
+b=2.2
+sum=a+b
+print(sum)"""
+#automatically
+
+"""#type casting
+a=10
+b=int(1.3)
+print(a+b)
+c=int("2")
+print(a+b+c)"""
+
+"""#taking input
+name=input("enter name: ")
+age=int(input("you age: "))
+print("helllo",name)
+print("you're", age)"""
+
+#input 2 numbers and sum
+"""a=int(input("Enter first"))
+b=int(input("enter second"))
+sum=a+b
+print("sum is: ", sum)
+"""
+
+"""#input l of square and find area
+l=int(input("enter length: " ))
+area=l**2
+print(area)
+"""
+
+"""#2 float and average
+a=float(input("enter first"))
+b=float(input("enter second"))
+print("average is ",(a+b)/2)"""
+
+#compare
+a=int(input("enter"))
+b=int(input("second"))
+print((a>=b))
